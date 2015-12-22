c0a155db2617d7ab194a9fd81feaf17baf3aa82f
bambooom <nmz89825@gmail.com>
2015-12-23 00:41:55 +0800

    add example file

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import json
+
+"""
+A example for creating a Table that is sortable by its header
+"""
+
+app = Flask(__name__)
+data = [{
+  "name": "bootstrap-table",
+  "commits": "10",
+  "attention": "122",
+  "uneven": "An extended Bootstrap table"
+},
+ {
+  "name": "multiple-select",
+  "commits": "288",
+  "attention": "20",
+  "uneven": "A jQuery plugin"
+}, {
+  "name": "Testing",
+  "commits": "340",
+  "attention": "20",
+  "uneven": "For test"
+}]
+
+
+#jdata=json.dumps(data)
+
+@app.route('/')
+def index():
+    return render_template("table.html", data=data)
+
+
+if __name__ == '__main__':
+	#print jdata
+  app.run(debug=True)
